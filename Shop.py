6ed6b678332bc819362fcd2f3717e9a2adbd2081
Loktev Alex <zil41047@mail.ru>
2022-05-26 23:28:25 +0400

    updates 26/05

--- Shop.py
+++ Shop.py
@@ -37,19 +37,42 @@ driver.implicitly_wait(5)
 # else:
 #     print("в категории не 3 товара")
 
-#сортировка товаров
+
+# #сортировка товаров
+# driver.get("http://practice.automationtesting.in/")
+# driver.find_element_by_link_text("My Account").click()
+# driver.find_element_by_id("username").send_keys("zil4104@mail.ru")
+# driver.find_element_by_id("password").send_keys("Ac!78978977")
+# driver.find_element_by_css_selector("input[value='Login']").click()
+# driver.find_element_by_css_selector("a[href='http://practice.automationtesting.in/shop/']").click()
+#
+# selector=driver.find_element_by_css_selector("option[value='menu_order']")
+# selector_status=selector.get_attribute("selected")
+# if selector_status == "true":
+#     print("в селекторе выбран вариант сортировки по умолчанию")
+# else:
+#     print("в селекторе выбран вариант сортировки НЕ по умолчанию")
+#
+# select=Select(driver.find_element_by_css_selector("select[class='orderby']"))
+# select.select_by_visible_text("Sort by price: high to low")
+#
+# selector=driver.find_element_by_css_selector("option[value='price-desc']")
+# selector_status=selector.get_attribute("selected")
+# if selector_status == "true":
+#     print("в селекторе выбран вариант сортировки от большей цены к меньшей")
+# else:
+#     print("в селекторе выбран вариант сортировки НЕ от большей цены к меньшей")
+
+
+#отображение, скидка товара
 driver.get("http://practice.automationtesting.in/")
 driver.find_element_by_link_text("My Account").click()
 driver.find_element_by_id("username").send_keys("zil4104@mail.ru")
 driver.find_element_by_id("password").send_keys("Ac!78978977")
 driver.find_element_by_css_selector("input[value='Login']").click()
 driver.find_element_by_css_selector("a[href='http://practice.automationtesting.in/shop/']").click()
-selector=driver.find_element_by_css_selector("option[value='menu_order']")
-selector_status=selector.get_attribute("selected")
-if selector_status == "true":
-    print("в селекторе выбран вариант сортировки по умолчанию")
-else:
-    print("в селекторе выбран вариант сортировки НЕ по умолчанию")
-element_1=driver.find_element_by_css_selector("select[class='woocommerce-ordering']")
-select=Select(element_1)
-select.select_by_visible_text("Sort by price: high to low")
+driver.find_element_by_css_selector("img[title='Android Quick Start Guide']").click()
+element=driver.find_elements_by_css_selector("p[class='price']")
+element_text=element.text
+assert element_text=="600"
+# assert element_text=="450"
